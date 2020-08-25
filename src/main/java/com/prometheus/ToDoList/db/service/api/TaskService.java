ac8c47b27df15feb217b0dc07100123590875ea4
MartinBugar <martin1.bugar@gmail.com>
2020-08-25 10:18:49 +0200

    services created

---
+++ src/main/java/com/prometheus/ToDoList/db/service/api/TaskService.java
@@ -0,0 +1,21 @@
+package com.prometheus.ToDoList.db.service.api;
+
+import com.prometheus.ToDoList.domain.Task;
+
+import java.util.List;
+
+public interface TaskService {
+
+    List<Task> getTasks(); // vracia vsetky tasky
+
+    Task get(Integer id); // vrati task na zaklade id
+
+    Integer add(Task task); // vracia vygenerovane id
+
+    void delete(Integer id); // vymaze task na zaklade jeho id
+
+    void update(Integer id, UpdateTaskRequest updateTaskRequest);
+
+
+
+}
---
+++ src/main/java/com/prometheus/ToDoList/db/service/api/UserService.java
@@ -0,0 +1,14 @@
+package com.prometheus.ToDoList.db.service.api;
+
+import com.prometheus.ToDoList.domain.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    List<User> getUsers (); // vrati vsetkych userov
+
+    User get(Integer id); // vrati customera na zaklade id
+
+    Integer add(User user); // prida usera a vrati jeho id
+}

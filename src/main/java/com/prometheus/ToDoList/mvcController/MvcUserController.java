272c34580110c6e57baa15a74fe2b8a94e105509
Martin <martin1.bugar@gmail.com>
2020-08-31 15:25:06 +0200

    users.html created

--- src/main/java/com/prometheus/ToDoList/mvcController/MvcUserController.java
+++ src/main/java/com/prometheus/ToDoList/mvcController/MvcUserController.java
@@ -2,28 +2,35 @@ package com.prometheus.ToDoList.mvcController;
 
 import com.prometheus.ToDoList.db.repository.UserRepository;
 import com.prometheus.ToDoList.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MvcUserController {
-
+    @Autowired(required=true)
+    @Qualifier(value="userRepository")
     UserRepository userRepository;
 
     public MvcUserController(UserRepository userRepository){
-        this.userRepository =userRepository;
+        this.userRepository = userRepository;
     }
 
-    @RequestMapping("/users")
+
+
+    @RequestMapping(value = ("/users"), method = RequestMethod.GET)
     public String getUserIndex(Model model){
         model.addAttribute("users", userRepository.getAll());
         return "users";
     }
+
+
 
 
 

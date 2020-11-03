7fc08b5d46aa231c2d47adfca464c8066409fa93
Martin <martin1.bugar@gmail.com>
2020-11-03 16:46:16 +0100

    pridany frontend

---
+++ src/main/resources/static/precache-manifest.81472387427e5ae58933876a82ef1f70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b43d4311c29b90b6a4c2471b89de26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c17bd4dd72310d86608",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "19ab61d558bfd8ab3417",
+    "url": "/static/css/main.9176b3fc.chunk.css"
+  },
+  {
+    "revision": "7c17bd4dd72310d86608",
+    "url": "/static/js/2.891024f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.891024f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ab61d558bfd8ab3417",
+    "url": "/static/js/main.a78f70d4.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);

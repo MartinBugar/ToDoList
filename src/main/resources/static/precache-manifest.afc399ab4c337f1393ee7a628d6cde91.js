97e8d4e64898452b118d9baaf07bc7ec7b1ec30f
prometheus9911 <martin1.bugar@gmail.com>
2020-11-05 16:08:41 +0100

    fronend fixed

---
+++ src/main/resources/static/precache-manifest.afc399ab4c337f1393ee7a628d6cde91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce9f6f208cfce6287f11ea3085a3f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5716ddd5b51bdf5e135",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "16d01755036f8e575838",
+    "url": "/static/css/main.ad0dc4c4.chunk.css"
+  },
+  {
+    "revision": "b5716ddd5b51bdf5e135",
+    "url": "/static/js/2.cc95a566.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.cc95a566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d01755036f8e575838",
+    "url": "/static/js/main.5e10e271.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
